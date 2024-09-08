6c96cb7140555c23bef10a4b1ba33d77414c7b28
KalimsCodingWonderland <159584563+KalimsCodingWonderland@users.noreply.github.com>
2024-09-08 14:53:38 -0400

    Added Home Module

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className='Home'>
+            <h1>Welcome to Algorithm Visualizer</h1>
+            <p>
+                We have all the algorithms you could ever need!
+                With the best UI you've ever seen!
+            </p>
+        </div>
+    );
+}
+
+export default Home;
