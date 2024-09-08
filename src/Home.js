6638e1ecf711ffbe50aa63e18b4d9c22ff5660d0
KalimsCodingWonderland <159584563+KalimsCodingWonderland@users.noreply.github.com>
2024-09-08 18:18:13 -0400

    Added Title Animation, and New Screen Scroll

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
 
 function Home() {
+    const [slideDown, setSlideDown] = useState(false);
+
+    function handleHeaderClick() {
+        setSlideDown(true);
+    }
+
+    let className = 'home-container';
+    if (slideDown) {
+        className += ' slide-down';
+    }
+
     return (
-        <div className='Home'>
-            <h1>Welcome to Algorithm Visualizer</h1>
-            <p>
-                We have all the algorithms you could ever need!
-                With the best UI you've ever seen!
-            </p>
+        <div className="home-wrapper">
+            <div className={className}>
+                <button className="home-header" onClick={handleHeaderClick}>
+                    <h1>P & Q <br /> ALGORITHM <br /> VISUALIZER</h1>
+                </button>
+            </div>
+            <div className={`second-screen ${slideDown ? 'show' : ''}`}>
+                <h2>P & Q</h2>
+                <h2>ALGORITHM <br/> VISUALIZER</h2>
+            </div>
         </div>
     );
 }
 
 export default Home;
+
